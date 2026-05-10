aadd21cf77ecf44a1e212e1b99640aa527d6bfed
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2026-05-09 23:01:02 -0300

    feat: add selectors list for login spec

--- cypress/cypress/e2e/login.spec.cy.js
+++ cypress/cypress/e2e/login.spec.cy.js
@@ -1,5 +1,30 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Orange HRM Tests", () => {
+  const selectorsList = {
+    username: '[name="username"]',
+    password: "[name='password']",
+    submit: "[type='submit']",
+    alert: "[role='alert']",
+    sectionTitleTopBar: "[class='oxd-topbar-header-breadcrumb-module']",
+  };
+
+  it("Login - Success", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.get(selectorsList.username).type("Admin");
+    cy.get(selectorsList.password).type("admin123");
+    cy.get(selectorsList.submit).click();
+    cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
+  });
+
+  it("Login - Fail", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+    cy.get(selectorsList.username).type("Test");
+    cy.get(selectorsList.password).type("Test");
+    cy.get(selectorsList.submit).click();
+    cy.get(selectorsList.alert);
+  });
+});
