0a204019e1f9f274a010fd17e48f4c92436592f9
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2026-05-13 21:39:12 -0300

    feat: add user update scenario

--- cypress/cypress/e2e/user.spec.cy.js
+++ cypress/cypress/e2e/user.spec.cy.js
@@ -10,7 +10,13 @@ describe("Orange HRM Tests", () => {
     dashboardGrid: "[class='oxd-layout-context']",
     myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
     firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
+    genericField: ".oxd-input--active",
+    dropDownInput: ".oxd-select-text--active",
+    dateField: "[placeholder='yyyy-mm-dd']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']",
   };
 
   it.only("User Info Success - Success", () => {
@@ -22,8 +28,27 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.dashboardGrid);
     cy.get(selectorsList.myInfoButton).click();
     cy.get(selectorsList.firstNameField, { timeout: 10000 }).type(
-      "NicknameTest",
+      "FirstNameTest",
     );
+    cy.get(selectorsList.middleNameField, { timeout: 10000 }).type(
+      "MiddleNameTest",
+    );
+    cy.get(selectorsList.lastNameField, { timeout: 10000 }).type(
+      "LastNameTest",
+    );
+    cy.get(selectorsList.genericField, { timeout: 10000 }).eq(3).type("123");
+    cy.get(selectorsList.genericField, { timeout: 10000 })
+      .eq(4)
+      .type("OtherIdTest");
+    cy.get(selectorsList.genericField, { timeout: 10000 })
+      .eq(5)
+      .type("DriversLicenseNumberTest");
+    cy.get(selectorsList.genericField, { timeout: 10000 })
+      .eq(6)
+      .type("2025-03-10");
+    cy.get(selectorsList.dateCloseButton).click();
+    cy.get(selectorsList.submitButton).eq(0).click();
+    cy.get("body").should("contain", "Successfully Updated");
   });
 
   it("Login - Fail", () => {
