20ab7b51a2c34068ef31525337f644c054fcdff2
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2026-05-15 07:39:42 -0300

    feat: add all fields in user spec

--- cypress/cypress/e2e/user.spec.cy.js
+++ cypress/cypress/e2e/user.spec.cy.js
@@ -17,6 +17,7 @@ describe("Orange HRM Tests", () => {
     dateField: "[placeholder='yyyy-mm-dd']",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
+    textInput: ".oxd-select-text-input",
   };
 
   it.only("User Info Success - Success", () => {
@@ -36,17 +37,26 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.lastNameField, { timeout: 10000 }).type(
       "LastNameTest",
     );
-    cy.get(selectorsList.genericField, { timeout: 10000 }).eq(3).type("123");
+    cy.get(selectorsList.genericField, { timeout: 10000 })
+      .eq(3)
+      .clear()
+      .type("123teste");
     cy.get(selectorsList.genericField, { timeout: 10000 })
       .eq(4)
       .type("OtherIdTest");
     cy.get(selectorsList.genericField, { timeout: 10000 })
       .eq(5)
+      .clear()
       .type("DriversLicenseNumberTest");
     cy.get(selectorsList.genericField, { timeout: 10000 })
       .eq(6)
+      .clear()
       .type("2025-03-10");
     cy.get(selectorsList.dateCloseButton).click();
+    cy.get(selectorsList.textInput).eq(0).click();
+    cy.contains("Brazilian").click();
+    cy.get(selectorsList.textInput).eq(1).click();
+    cy.contains("Other").click();
     cy.get(selectorsList.submitButton).eq(0).click();
     cy.get("body").should("contain", "Successfully Updated");
   });
