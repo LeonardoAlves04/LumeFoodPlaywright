7ef54cbe6149e5de4699372ad35dfa53680f9c5b
Leonardo Alves de Almeida <leonardoalves201510@gmail.com>
2026-05-20 07:16:30 -0300

    feat: add myInfoPage (page object) to automation

--- cypress/cypress/e2e/user.spec.cy.js
+++ cypress/cypress/e2e/user.spec.cy.js
@@ -2,25 +2,14 @@ import userData from "../fixtures/user-data.json";
 import LoginPage from "./../pages/loginPage";
 import DashboardPage from "./../pages/dashboardPage";
 import MenuPage from "../pages/menuPage";
+import MyInfoPage from "../pages/myInfoPage";
 
 const loginPage = new LoginPage();
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
+const myInfoPage = new MyInfoPage();
 
 describe("Orange HRM Tests", () => {
-  const selectorsList = {
-    firstNameField: "[name='firstName']",
-    middleNameField: "[name='middleName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input--active",
-    dropDownInput: ".oxd-select-text--active",
-    dateField: "[placeholder='yyyy-mm-dd']",
-    dateCloseButton: ".--close",
-    submitButton: "[type='submit']",
-    textInput: ".oxd-select-text-input",
-    radioInput: ".oxd-radio-input",
-  };
-
   it.only("User Info Success - Success", () => {
     loginPage.acessLoginPage();
     loginPage.loginWithUser(
@@ -28,41 +17,22 @@ describe("Orange HRM Tests", () => {
       userData.userSuccess.password,
     );
     dashboardPage.checkDashboardPage();
+
     menuPage.acessMyInfo();
 
-    cy.get(selectorsList.firstNameField, { timeout: 10000 })
-      .clear()
-      .type("FirstNameTest");
-    cy.get(selectorsList.middleNameField, { timeout: 10000 })
-      .clear()
-      .type("MiddleNameTest");
-    cy.get(selectorsList.lastNameField, { timeout: 10000 })
-      .clear()
-      .type("LastNameTest");
-    cy.get(selectorsList.genericField, { timeout: 10000 })
-      .eq(3)
-      .clear()
-      .type("123teste");
-    cy.get(selectorsList.genericField, { timeout: 10000 })
-      .eq(4)
-      .clear()
-      .type("OtherIdTest");
-    cy.get(selectorsList.genericField, { timeout: 10000 })
-      .eq(5)
-      .clear()
-      .type("DriversLicenseNumberTest");
-    cy.get(selectorsList.genericField, { timeout: 10000 })
-      .eq(6)
-      .clear()
-      .type("2025-03-10");
-    cy.get(selectorsList.dateCloseButton).click();
-    cy.get(selectorsList.textInput).eq(0).click();
-    cy.contains("Brazilian").click();
-    cy.get(selectorsList.textInput).eq(1).click();
-    cy.contains("Other").click();
-    cy.get(selectorsList.radioInput).eq(1).click();
-    cy.get(selectorsList.submitButton).eq(0).click();
-    cy.get("body").should("contain", "Successfully Updated");
+    myInfoPage.fillPersonalDetails(
+      "TestFirstName",
+      "TestMiddleName",
+      "TestLastName",
+    );
+    myInfoPage.fillEmploymentDetails(
+      "12345",
+      "67890",
+      "A1234567",
+      "2024-01-01",
+    );
+    myInfoPage.fillStatusDetails();
+    myInfoPage.saveForm();
   });
 
   it("Login - Fail", () => {
